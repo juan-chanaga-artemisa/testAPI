7cc1bd398f6a675447991a9071185f1414be3b35
Juan Chanagá <juan.chanaga@artemisa.tech>
2024-11-06 23:33:52 -0500

    Segundo commit

--- src/main/java/com/example/SampleService/controller/HelloController.java
+++ src/main/java/com/example/SampleService/controller/HelloController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @GetMapping("/")
+    public String pingBack() {
+        return "The back is running";
+    }
+
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello, this is a simple response from the Java service!";
